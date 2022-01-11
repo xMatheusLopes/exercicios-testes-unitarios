2e43a49c0de69d190516ea5fbc7bc862683f52f2
Matheus Lopes <mhenrilo@emeal.nttdata.com>
2022-01-11 18:00:55 -0300

    Exercicios adicionados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,27 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'exercicios';
 
+  arrayNumbers: number[] = [];
+
   sum(n1: number, n2: number): number {
     return n1 + n2;
+  }
+
+  // Checa se a idade da pessoa é considerado de maior ou de menor
+  checkOfAge(age: number): boolean {
+    if (age >= 18) {
+      return true;
+    }
+
+    return false;
+  }
+
+  // Incrementa em 1 cada item no array
+  incrementItemsInArray(): void {
+    if (this.arrayNumbers.length) {
+      this.arrayNumbers = this.arrayNumbers.map((item) => {
+        return item + 1;
+      });
+    }
   }
 }
